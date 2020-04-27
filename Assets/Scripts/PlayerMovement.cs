5675ab78ececd2c06dba93122d2966ba418c69b1
Darren Lee Ferrer <darrenferrer888@yahoo.com>
2020-04-26 20:49:56 -0700

    Fixed movement forever going in one direction & added gravity

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 moveVector;
+    public CharacterController controller;
+
+    public float speed = 5.0f;
+    public float verticalVelocity = 0.0f;
+    private float gravity = 10.0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = 0.0f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
