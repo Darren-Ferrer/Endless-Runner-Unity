1c593fd66cd04cf1510ccaa86d3b78f16d1daba5
Darren Lee Ferrer <darrenferrer888@yahoo.com>
2020-04-29 01:29:00 -0700

    Added collider triggers and a retry screen

---
+++ Assets/Scripts/RetryScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryScreen : MonoBehaviour
+{
+    void OnTriggerEnter(Collider die)
+    {
+        Destroy(die.gameObject);
+        SceneManager.LoadScene("RetryScreen");
+    }
+
+    public void toMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void toGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
