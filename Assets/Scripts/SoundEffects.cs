170592efe8b9e64af1b86261bcef0a66611709f1
Darren Lee Ferrer <darrenferrer888@yahoo.com>
2020-05-09 01:57:13 -0700

    imported some sounds

---
+++ Assets/Scripts/SoundEffects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+    public AudioSource niceTry;
+    public AudioSource welcome;
+    public AudioSource running;
+
+    public void playNiceTry()
+    {
+        niceTry.play();
+    }
+
+    public void playWelcome()
+    {
+        welcome.play();
+    }
+
+    public void playRunning()
+    {
+        running.play();
+    }
+}
